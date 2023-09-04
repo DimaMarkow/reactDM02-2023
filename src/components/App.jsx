afbadf64528df114d1efa21e2bffc572f890a564
DimaMarkow <dimamarkow@gmail.com>
2023-09-04 15:04:22 +0300

    PlusHeader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,80 +1,47 @@
 import { Component } from 'react';
-import FeedbackButton from './FeedbackButton/FeedbackButton';
-import Section from './Section/Section';
-import Statistics from './Statistics/Statistics';
-import Notification from './Notification';
-
-const INITIAL_STATE = {
-  good: 0,
-  neutral: 0,
-  bad: 0,
-};
+import Feedback from './Feedback';
 
 class App extends Component {
-  state = {
-    ...INITIAL_STATE,
-  };
-
-  handleClick = e => {
-    this.setState(prevState => {
-      return {
-        [e.target.name]: prevState[e.target.name] + 1,
-      };
-    });
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    return total;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
-    if (!total) {
-      return 0;
-    }
-    const value = this.state.good;
-    const result = ((value / total) * 100).toFixed(0);
-    return Number(result);
-  };
-
   render() {
-    const totalStat = this.countTotalFeedback();
-
     return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Section title="Please leave feedback">
-          <FeedbackButton handleClick={this.handleClick} />
-        </Section>
-        {totalStat ? (
-          <Section title="Statistics">
-            <Statistics
-              date={this.state}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          </Section>
-        ) : (
-          <Section title="Statistics">
-            <Notification message="There is no feedback" />
-          </Section>
-        )}
-        {/* {!totalStat && (
-          <Section title="Statistics">
-            <Notification message="There is no feedback" />
-          </Section>
-        )} */}
+      <div>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              Homework-DM-02
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <form className="container-fluid justify-content-start">
+                <button
+                  className="btn btn-sm btn-outline-success me-2"
+                  type="button"
+                >
+                  Feedback
+                </button>
+                <button
+                  className="btn btn-sm btn-outline-secondary"
+                  type="button"
+                >
+                  Contacts
+                </button>
+              </form>
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
---
+++ src/components/Feedback.jsx
@@ -0,0 +1,68 @@
+import { Component } from 'react';
+import FeedbackButton from './FeedbackButton/FeedbackButton';
+import Section from './Section/Section';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification';
+
+const INITIAL_STATE = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
+class Feedback extends Component {
+  state = {
+    ...INITIAL_STATE,
+  };
+
+  handleClick = e => {
+    this.setState(prevState => {
+      return {
+        [e.target.name]: prevState[e.target.name] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    if (!total) {
+      return 0;
+    }
+    const value = this.state.good;
+    const result = ((value / total) * 100).toFixed(0);
+    return Number(result);
+  };
+
+  render() {
+    const totalStat = this.countTotalFeedback();
+
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackButton handleClick={this.handleClick} />
+        </Section>
+        {totalStat ? (
+          <Section title="Statistics">
+            <Statistics
+              date={this.state}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          </Section>
+        ) : (
+          <Section title="Statistics">
+            <Notification message="There is no feedback" />
+          </Section>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Feedback;
